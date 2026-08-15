9a436f6bab3e25ac62353edbdb7b98591228d11a
ahmadjqjx <sajdabanoff@gmail.com>
2026-08-15 13:49:52 -0700

    for  learning a js

---
+++ index.js
@@ -0,0 +1,32 @@
+alert("i am link");
+//loop
+
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+for(let i=0;i<5;i++){
+    console.log("hello world", i);
+}
+
+for(let i=1;i<=100;i=i+2){
+    console.log(i)
+}
+
+let j=1;
+while(j<=100){
+        console.log(i)
+        j=j+2;
+
+}
+
+let name = prompt("enter your name:")
+console.log(name){
+}
+
+while (name.length<3){
+    name=prompt("enter your correct name:")
+}
+ 
+
+
+let username = 
